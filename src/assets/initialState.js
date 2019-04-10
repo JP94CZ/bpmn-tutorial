0ef3360b1645d1b4260e8f3c5fbf8066e51685a8
Jan Polan <polanja6@fel.cvut.cz>
2019-04-10 15:29:36 +0200

    initial state exported to assets file

---
+++ src/assets/initialState.js
@@ -0,0 +1,88 @@
+import naplava from './pictures/pavel_naplava.png';
+import zoubek from './pictures/lukas_zoubek.jpg';
+import koci from './pictures/jan_koci.jpg';
+import gorecki from './pictures/ales_gorecki.jpg';
+import malinkovic from './pictures/tomas-malinkovic2.png';
+
+export const initialState = {
+    chapters: [
+      {
+        name: 'Tutorial',
+        finished: false,
+        conversations: [
+          {
+            photo: naplava,
+            name: 'Pavel Náplava',
+            title: 'BPMN Master',
+            conversation: "pavelNaplava",
+            id: 0,
+            conversationProgress: [],
+            conversationFinalLength: null,
+            finished: false
+          },
+          {
+            photo: malinkovic,
+            name: `Tomáš Malinkovič`,
+            title: 'BPM Chief',
+            conversation: "tomasMalinkovic",
+            id: 1,
+            conversationProgress: [],
+
+            finished: false
+          }
+        ]
+      },
+      {
+        name: 'Chapter 1',
+        finished: false,
+        conversations: [
+
+          {
+            photo: koci,
+            name: `Jan Kočí`,
+            title: 'Big Boss',
+            conversation: "janKoci",
+            id: 0,
+            conversationProgress: [],
+            finished: false
+          }
+        ]
+      },
+      {
+        name: 'Chapter 2',
+        finished: false,
+        conversations: [
+
+          {
+            photo: zoubek,
+            name: `Lukáš Zoubek`,
+            title: 'Lil\' Boss',
+            conversation: "lukasZoubek",
+            id: 0,
+            conversationProgress: [],
+            finished: false
+          }
+        ]
+      },
+      {
+        name: 'Chapter 3',
+        finished: false,
+        conversations: [
+          {
+            photo: gorecki,
+            name: `Aleš Górecki`,
+            title: 'Moodle Captain',
+            conversation: "alesGorecki",
+            id: 0,
+            conversationProgress: [],
+            finished: false
+          }
+        ]
+      }
+    ]
+    ,
+    actualConversation: 0,
+    actualChapter: 0,
+    availableChaptersAmount: 1,
+    displayDraw: false
+  }
--- src/components/Messenger/index.js
+++ src/components/Messenger/index.js
@@ -8,91 +8,11 @@ import zoubek from '../../assets/pictures/lukas_zoubek.jpg';
 import koci from '../../assets/pictures/jan_koci.jpg';
 import gorecki from '../../assets/pictures/ales_gorecki.jpg';
 import malinkovic from '../../assets/pictures/tomas-malinkovic2.png';
+import {initialState} from '../../assets/initialState.js';
 
 export default class Messenger extends Component {
 
-  state = {
-    chapters: [
-      {
-        name: 'Tutorial',
-        finished: false,
-        conversations: [
-          {
-            photo: naplava,
-            name: 'Pavel Náplava',
-            title: 'BPMN Master',
-            conversation: "pavelNaplava",
-            id: 0,
-            conversationProgress: [],
-            conversationFinalLength: null,
-            finished: false
-          },
-          {
-            photo: malinkovic,
-            name: `Tomáš Malinkovič`,
-            title: 'BPM Chief',
-            conversation: "tomasMalinkovic",
-            id: 1,
-            conversationProgress: [],
-
-            finished: false
-          }
-        ]
-      },
-      {
-        name: 'Chapter 1',
-        finished: false,
-        conversations: [
-
-          {
-            photo: koci,
-            name: `Jan Kočí`,
-            title: 'Big Boss',
-            conversation: "janKoci",
-            id: 0,
-            conversationProgress: [],
-            finished: false
-          }
-        ]
-      },
-      {
-        name: 'Chapter 2',
-        finished: false,
-        conversations: [
-
-          {
-            photo: zoubek,
-            name: `Lukáš Zoubek`,
-            title: 'Lil\' Boss',
-            conversation: "lukasZoubek",
-            id: 0,
-            conversationProgress: [],
-            finished: false
-          }
-        ]
-      },
-      {
-        name: 'Chapter 3',
-        finished: false,
-        conversations: [
-          {
-            photo: gorecki,
-            name: `Aleš Górecki`,
-            title: 'Moodle Captain',
-            conversation: "alesGorecki",
-            id: 0,
-            conversationProgress: [],
-            finished: false
-          }
-        ]
-      }
-    ]
-    ,
-    actualConversation: 0,
-    actualChapter: 0,
-    availableChaptersAmount: 1,
-    displayDraw: false
-  }
+  state = initialState;
 
   getAvailableChaptersNames = () => {
     let chapterNames = [];
